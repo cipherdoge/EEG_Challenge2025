35a67a9c4ae92119527ca2a501ed439e0cd84b68
cipherdoge <ritviksharma99@gmail.com>
2025-07-15 22:39:01 +0530

    removed hover

--- script.js
+++ script.js
@@ -2,14 +2,6 @@ document.addEventListener("DOMContentLoaded", () => {
   const link = document.getElementById("viz-link");
   const message = document.getElementById("message");
 
-  link.addEventListener("mouseover", () => {
-    message.textContent = "You're about to see the magic!";
-  });
-
-  link.addEventListener("mouseout", () => {
-    message.textContent = "";
-  });
-
   link.addEventListener("click", () => {
     message.textContent = "Loading brain visualization...";
   });
